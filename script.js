002f9b65192eb94e1e7b1edad45853df11a709b8
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2026-02-22 07:57:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,51 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((emp) => {
+	if(emp.profession === "developer"){
+		console.log(emp);
+	}
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((emp) => {
+	if(emp.profession === "developer"){
+		console.log(emp);
+	}
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newEmp = {
+	  id : 4,
+	  name : "Susan",
+	  age : "20",
+	  profession : "intern",
+  };
+  
+  arr.push(newEmp);
+  console.log(arr);
+	  
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let updatedArray = arr.filter(
+	(emp) => emp.profession !== "admin"
+	);
+	console.log(updatedArray);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+	{id: 5, name: "Loki", age:"22", profession: "developer"},
+	{id: 6, name: "Sandy", age:"25", profession: "admin"},
+	{id: 7, name: "Pratap", age:"22", profession: "intern"},
+  ];
+  let combined = arr.concat(newArr);
+  console.log(combined);
 }
